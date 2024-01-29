ee872c1f0b2df9c62c98ea9a2d841db410640c70
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2024-01-29 13:43:48 +0530

    created client3 chat application

---
+++ Group-Chat1/Client3/src/main/java/lk/ijse/Client3_Initializer.java
@@ -0,0 +1,25 @@
+
+    package lk.ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+    public class Client3_Initializer extends Application {
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            Parent root = FXMLLoader.load(this.getClass().getResource("/View/Client1LoginForm.fxml"));
+            Scene mainScene = new Scene(root);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Chat Room");
+            primaryStage.centerOnScreen();
+            primaryStage.show();
+        }
+    }
+
---
+++ Group-Chat1/Client3/src/main/java/lk/ijse/Client3_InitializerWrapper.java
@@ -0,0 +1,7 @@
+package lk.ijse;
+
+public class Client3_InitializerWrapper {
+    public static void main(String[] args) {
+        Client3_Initializer.main(args);
+    }
+}
